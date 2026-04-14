06c896141c8e0c8b602b00a88e549374e33e4395
Akash Singh Patel <akashpatelsingh9893693091@gmail.com>
2026-04-14 19:54:01 +0530

    fix: adjust positioning of wingo container by replacing padding with top offset

--- app/wingo.tsx
+++ app/wingo.tsx
@@ -3434,7 +3434,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     alignItems: "center",
-    paddingTop: "142%",
+    top: "59%",
   },
   winLossTitle: {
     fontSize: wp(10),
